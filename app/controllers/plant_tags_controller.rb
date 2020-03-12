39d2c6e6b34cf450281b21d5f645afa59f2dd2f7
Andre Ferrer <andrerferrer@hotmail.com>
2020-03-12 14:46:09 -0300

    refactor it a little bit

--- app/controllers/plant_tags_controller.rb
+++ app/controllers/plant_tags_controller.rb
@@ -6,36 +6,22 @@ class PlantTagsController < ApplicationController
 
   def create
     @plant = Plant.find params[:plant_id]
-
-    # this was the old way
-    # @tag = Tag.find params.require(:plant_tag).permit(:tag)[:tag]
-
     @tags = Tag.find params[:plant_tag][:tag_id]
 
     @tags.each do |tag|
-      @plant_tag = PlantTag.new
-      @plant_tag.plant = @plant
-      @plant_tag.tag = tag
-      @plant_tag.save
+      plant_tag = PlantTag.new(plant: @plant, tag: tag)
+      plant_tag.save
     end
 
-    # we're doing like this
-
-    # this is not needed like this
-    # @plant_tag.plant_id = @plant.id
-    # this is no longer need
-    # @plant_tag.tag_id = @tag.id
-
-    # if @plant_tag.save
     redirect_to garden_path(@plant.garden.id)
+    # old create for only one tag
+    # @plant = Plant.find(params[:plant_id])
+    # @tag = Tag.find(params[:plant_tag][:tag_id])
+    # @plant_tag = PlantTag.new(plant: @plant, tag: @tag)
+    # if @plant_tag.save
+    #   redirect_to garden_path(@plant.garden)
     # else
-      # render :new
+    #   render :new
     # end
   end
-
-  private
-
-  # def plant_tag_params
-  #   params.require(:plant_tag).permit(:tag_id)
-  # end
 end
--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,7 +2,10 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2();
+  const select2Input = $('.select2');
+  if (select2Input) {
+    select2Input.select2();
+  };
 };
 
 export { initSelect2 };
